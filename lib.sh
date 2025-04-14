60eca6886ba2bf61f7c960057f2b77f93146d5a8
Jack Clarke <jackojc@gmail.com>
2025-04-14 15:13:44 +0100

    Fix scripts

--- lib.sh
+++ lib.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Constants
 HOMEDIR=$(getent passwd jack | cut -d: -f6)
--- stow.sh
+++ stow.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 . ./lib.sh
+
+# Install minimal shell environment
+. "${HOME}/.nix-profile/etc/profile.d/nix.sh"  # Source env vars for nix
 
 stow --no-folding .
