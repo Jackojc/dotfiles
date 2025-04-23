3b32b324b3b88ab41946ba692e10baf7f3e90ba6
Jack Clarke <jackojc@gmail.com>
2025-04-23 16:18:39 +0100

    Add individual scripts for stowing

---
+++ stow-shell.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+. ./lib.sh
+
+# Install minimal shell environment
+. "${HOME}/.nix-profile/etc/profile.d/nix.sh"  # Source env vars for nix
+
+stow --no-folding dots-common
+stow --no-folding dots-shell
