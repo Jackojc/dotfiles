2eecabf2df287e4ab492d4337389d0b9ea4e01df
Jack Clarke <jackojc@gmail.com>
2025-07-20 22:40:40 +0100

    Fix outdated directory names

--- stow-gui.sh
+++ stow-gui.sh
@@ -5,5 +5,5 @@
 # Install minimal shell environment
 . "${HOME}/.nix-profile/etc/profile.d/nix.sh"  # Source env vars for nix
 
-stow --no-folding dots-common
-stow --no-folding dots-gui
+stow --no-folding common
+stow --no-folding gui
